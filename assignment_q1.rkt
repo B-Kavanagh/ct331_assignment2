e61a4f7c8f2d6b0484fcdbe824a29d58c3d2f2fc
B-Kavanagh <B.kavanagh8@nuigalway.ie>
2017-10-26 23:28:28 +0100

    Update assignment_q1.rkt

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,19 @@
 #lang racket
+
+; A cons pair of two numbers
+(cons 4 5)
+
+; A list of 3 numbers, using only the cons function
+(cons 9(cons 6(cons 5'())))
+
+; A list containing string, with a number and nested list of 3 numbers
+; Using cons
+(cons '(7 8 9 )(cons "Test-String"(cons 3'())))
+
+; A list containing string, with a number and nested list of 3 numbers
+; Using lists
+(list (list 9 6 5) 3 (list "Test-String-2"))
+
+; A list containing string, with a number and nested list of 3 numbers
+; Using append
+(append '(1 2) '("Test-String-3") '(5))
