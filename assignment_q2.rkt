f0c3a7ff646b508e9a4e8e36c514627cef899895
B-Kavanagh <B.kavanagh8@nuigalway.ie>
2017-10-26 23:36:02 +0100

    Update assignment_q2.rkt

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,55 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
 (provide ins_beg)
 
 (define (ins_beg el lst)
   (display "Hello, I'm ins_beg!\n"))
+
+(provide ins_beg)
+(provide ins_end)
+(provide count_top_level)
+(provide count_instances)
+
+;a)
+(define (ins_beg el lst)
+(cons el lst))
+
+;b)
+(define (ins_end el lst)
+  (append lst (list el)))
+
+;c)
+(define (count_top_level myList)
+  (cond ((null? myList) 0)
+      ((not (pair? myList)) 1)
+      (else (+ (count_top_level (car myList))
+                 (count_top_level (cdr myList))))
+      )
+)
+
+;d)
+(define (count_instances item myList) 
+  (cond [(null? myList) 0]
+        [(equal? item (car myList)) (+ 1 (count_instances item (cdr myList)))] 
+        [(count_instances item (cdr myList))]
+        ) 
+)
+
+;e)
+(define (count_instances_tr el lst)
+  (tinst el 0 lst)
+ )
+
+(define (tinst el total lst)
+  (cond ((null? lst)0)
+        ((equal? el (car lst))(+ 1 total(tinst el total (cdr lst))))
+        ((tinst el total (cdr lst)))
+    )
+)
+
+;f)
+(define (count_instances_deep el lst)
+  (cond [(empty? lst) 0]
+        [(list? (car lst)) (+ (count_instances_deep el (car lst)) (count_instances_deep el (cdr lst)))]
+        [(equal? el (car lst)) (+ 1 (count_instances_deep el (cdr lst)))]
+[else (count_instances_deep el (cdr lst))]))
